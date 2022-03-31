33611ad508bc8ca3ab97c29b2f846a101601a7d2
Christian Madajski <cmad7317@tutanota.com>
2022-03-31 00:28:19 -0400

    add flash to provide resend activation code feedback

--- src/main.py
+++ src/main.py
@@ -232,7 +232,7 @@ def validate():
             # change account validation status to true
             requested_user.active = True
             db.session.commit()
-            flash('Account for email ' + requested_user.email + ' has been successfully activated!')
+            flash('Account for ' + requested_user.email + ' has been successfully activated!')
             return redirect(url_for('index'))
 
 
@@ -296,6 +296,7 @@ def resend_validation():
                 server.login(sender_email, gmail_password)
                 # send email to user
                 server.sendmail(sender_email, receiver_email, email_content)
+            flash('A new activation code has been sent to email ' + given_email)
             return redirect(url_for('validate'))
 
 
