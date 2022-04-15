4b237fb57d01ee9892466b6d68343fbf577a4784
Drew <dcm752052@gmail.com>
2022-04-14 21:39:22 -0400

    moved js functions inside ready method. changed how product_feed displays user's search querys

--- src/static/javascript/scripts.js
+++ src/static/javascript/scripts.js
@@ -1,17 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navLinks.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-links li a").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navLinks.classList.remove("active");
-}))
-
 $(document).ready(function() {
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navLinks.classList.toggle("active");
+    });
+
+    document.querySelectorAll(".nav-links li a").forEach(n => n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navLinks.classList.remove("active");
+    }));
+
     $(".select-category").change(function(){
         $("#widthTempOption").html($('.select-category option:selected').text());
         $(this).width($("#selectTagWidth").width()+31);
