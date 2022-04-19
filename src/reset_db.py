87667b22f741634cfea83214795c2c3882b6a380
Christian Madajski <cmad7317@tutanota.com>
2022-04-19 16:12:56 -0400

    add phone # column to DB

--- src/reset_db.py
+++ src/reset_db.py
@@ -9,11 +9,11 @@ def reset_db():
     # add default accounts for developers
     # create User object with developer info
     christian = User(name='Christian Madajski', id=1234, email=
-    'cmadajsk@uncc.edu', password='password2', validation_code='123456', active=True)
+    'cmadajsk@uncc.edu', phone='980-290-0679', password='password2', validation_code='123456', active=True)
     oviya = User(name='Oviya Monoharan', id=704, email=
-    'omanohar@uncc.edu', password='oviya123', validation_code='123456', active=True)
+    'omanohar@uncc.edu', phone='111-222-3333', password='oviya123', validation_code='123456', active=True)
     hinal = User(name='Hinal Makadiya', id=111, email=
-    'hmakadi1@uncc.edu', password='Niner@123', validation_code='123456', active=True)
+    'hmakadi1@uncc.edu', phone='1234567890', password='Niner@123', validation_code='123456', active=True)
     professor = User(name='Dr. Rudd', id=1357, email=
     'jrudd@uncc.edu', password='bestprofessor1234', validation_code='123456', active=True)
     praveen = User(name='Praveen Kumar Gavara', id=2468, email=
